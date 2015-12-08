e98e01df72c9cb872b91a42f61dc9798e92b7384
ekzhu <ekzhu@cs.toronto.edu>
2015-12-08 16:35:19 -0500

    add bruteforce

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 
 using std::cin;
 
+
+
+
 int main(int argc, char *argv[]) {
     std::ifstream is(argv[1], std::ifstream::binary);
 
@@ -24,8 +27,36 @@ int main(int argc, char *argv[]) {
 	        std::cout << " ";
 	    }
 	}
+
 	delete[] buffer;
 
     }
+
+
+    int nn = 3;
+
+    Matrix<float> dataset;
+    Matrix<float> query;
+    load_from_file(dataset, "dataset.hdf5","dataset");
+    load_from_file(query, "dataset.hdf5","query");
+
+    Matrix<int> indices(new int[query.rows*nn], query.rows, nn);
+    Matrix<float> dists(new float[query.rows*nn], query.rows, nn);
+
+    // construct an randomized kd-tree index using 4 kd-trees
+    Index<L2<float> > index(dataset, flann::KDTreeIndexParams(4));
+    index.buildIndex();                                                                                               
+
+    // do a knn search, using 128 checks
+    index.knnSearch(query, indices, dists, nn, flann::SearchParams(128));
+
+    flann::save_to_file(indices,"result.hdf5","result");
+
+    delete[] dataset.ptr();
+    delete[] query.ptr();
+    delete[] indices.ptr();
+    delete[] dists.ptr();
+
+
     return 0;
 }
