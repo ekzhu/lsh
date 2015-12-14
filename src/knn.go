bcd6f91e4ef66068649db67408b801956984ae7d
ekzhu <ekzhu@cs.toronto.edu>
2015-12-14 16:50:25 -0500

    add exact knn

---
+++ src/knn.go
@@ -0,0 +1,79 @@
+package lsh
+
+import "container/heap"
+
+type Candidate struct {
+	id       int
+	distance float64
+}
+
+type KHeap struct {
+	k          int
+	candidates []Candidate
+}
+
+func (h KHeap) Len() int {
+	return len(h.candidates)
+}
+
+func (h KHeap) Less(i, j int) bool {
+	// We want to pop out the candidate with largest distance
+	// so we use greater than here
+	return h.candidates[i].distance > h.candidates[j].distance
+}
+
+func (h KHeap) Swap(i, j int) {
+	h.candidates[i], h.candidates[j] = h.candidates[j], h.candidates[i]
+}
+
+func (h *KHeap) Push(x interface{}) {
+	c := x.(Candidate)
+	if len(h.candidates) < h.k {
+		h.candidates = append(h.candidates, c)
+		return
+	}
+	// Check if we can still push to the top-k heap when it is full
+	if h.candidates[0].distance > c.distance {
+		heap.Pop(h)
+		heap.Push(h, c)
+	}
+	// sliently return when push is not possible
+	return
+}
+
+func (h *KHeap) Pop() interface{} {
+	k := len(h.candidates)
+	x := h.candidates[k-1]
+	h.candidates = h.candidates[0 : k-1]
+	return x
+}
+
+func NewKHeap(k int) *KHeap {
+	return &KHeap{k, make([]Candidate, 0)}
+}
+
+type Knn struct {
+	data []Point
+	ids  []int
+}
+
+func NewKnn(data []Point, ids []int) *Knn {
+	if len(data) != len(ids) {
+		panic("Mismatch between size of data and ids")
+	}
+	return &Knn{data, ids}
+}
+
+// Query outputs the top-k closest points from the query point
+// to the chanel out. The sequence of output is NOT sorted by
+// distance.
+func (knn *Knn) Query(q Point, k int, out chan int) {
+	kheap := NewKHeap(k)
+	for i, p := range knn.data {
+		d := p.l2(q)
+		heap.Push(kheap, Candidate{knn.ids[i], d})
+	}
+	for i := range kheap.candidates {
+		out <- kheap.candidates[i].id
+	}
+}
---
+++ src/knn_test.go
@@ -0,0 +1,79 @@
+package lsh
+
+import (
+	"container/heap"
+	"sort"
+	"testing"
+)
+
+func Test_KHeap(t *testing.T) {
+	points := randomPoints(10, 10, 100.0)
+	k := 5
+	h := NewKHeap(k)
+	heap.Init(h)
+	q := points[0]
+	distances := make([]float64, len(points))
+	for i := range points {
+		distances[i] = points[i].l2(q)
+		c := Candidate{i, distances[i]}
+		heap.Push(h, c)
+		t.Log(c)
+		t.Log(h.candidates)
+	}
+	if len(h.candidates) != k {
+		t.Error("Heap failed to maintain correct number of items")
+	}
+	sort.Float64s(distances)
+	topK := make([]float64, k)
+	for i := 0; i < k; i++ {
+		c := heap.Pop(h).(Candidate)
+		topK[i] = c.distance
+	}
+	for i := range topK {
+		if topK[i] != distances[k-1-i] {
+			t.Errorf("Expected order (reverse it) <%s>\nActual order <%s>\n",
+				distances[:k], topK)
+		}
+	}
+}
+
+func Test_Knn(t *testing.T) {
+	k := 5
+	points := randomPoints(20, 10, 100.0)
+	ids := make([]int, len(points))
+	for i := range points {
+		ids[i] = i
+	}
+	q := points[0]
+	// Build ground truth
+	distances := make([]float64, len(points))
+	for i := range points {
+		distances[i] = points[i].l2(q)
+	}
+	sort.Float64s(distances)
+	t.Log("Ground truth distances", distances[:k])
+	// Test Knn query
+	knn := NewKnn(points, ids)
+	out := make(chan int)
+	go func() {
+		knn.Query(q, k, out)
+		close(out)
+	}()
+	for id := range out {
+		// get the point
+		p := points[id]
+		// get the distance
+		d := p.l2(q)
+		t.Log(d)
+		// check if the distance is indeed within the top k ground truth
+		found := false
+		for i := 0; i < k; i++ {
+			if d == distances[i] {
+				found = true
+			}
+		}
+		if !found {
+			t.Error("Query did not find top k ground truth distance")
+		}
+	}
+}
