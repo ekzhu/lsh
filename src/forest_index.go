12eb84e7071c7a5cf5b8952a450afff9ee488ca6
Charlie Mei <meicharlie@gmail.com>
2015-12-14 17:55:31 -0500

    Query for forest

--- src/forest_index.go
+++ src/forest_index.go
@@ -2,11 +2,37 @@ package lsh
 
 type TreeNode struct {
 	// Hash key for this intermediate node. nil/empty for root nodes.
-	hashKey TableKey
+	hashKey int
 	// A list of indices to the source dataset.
 	indices []int
-	// Child nodes.
-	children []TreeNode
+	// Child nodes, keyed by partial hash value.
+	children map[int]*TreeNode
+}
+
+// recursiveAdd recurses down the tree to find the correct location to insert id.
+// Returns whether a new hash value was added.
+func (node *TreeNode) recursiveAdd(level int, id int, tableKey TableKey) bool {
+	if level == len(tableKey) {
+		node.indices = append(node.indices, id)
+		return false
+	} else {
+		// Check if next hash exists in children map. If not, create.
+		var next *TreeNode
+		hasNewHash := false
+		if node, ok := node.children[tableKey[level]]; !ok {
+			next = &TreeNode{
+				hashKey:  tableKey[level],
+				indices:  make([]int, 0),
+				children: make(map[int]*TreeNode),
+			}
+			node.children[tableKey[level]] = next
+			hasNewHash = true
+		} else {
+			next = node
+		}
+		// Recurse using next level's hash value.
+		return hasNewHash || next.recursiveAdd(level+1, id, tableKey)
+	}
 }
 
 type Tree struct {
@@ -16,27 +42,58 @@ type Tree struct {
 	root TreeNode
 }
 
+func (tree *Tree) insertIntoTree(id int, tableKey TableKey) {
+	if tree.root.recursiveAdd(0, id, tableKey) {
+		tree.count++
+	}
+}
+
 type ForestIndex struct {
 	// Embedded type
 	*LshSettings
-	// Number of leaves in the tree.
-	count int
 	// Trees.
 	trees []Tree
 }
 
 func NewLshForest(dim, l, m int, w float64) *ForestIndex {
 	trees := make([]Tree, l)
+	for _, treeRoot := range trees {
+		treeRoot.root = TreeNode{
+			indices:  make([]int, 0),
+			children: make(map[int]*TreeNode),
+		}
+	}
 	return &ForestIndex{
 		LshSettings: NewLshSettings(m, l, dim, w),
-		count:       0,
 		trees:       trees,
 	}
 }
 
 // Inserts a point into the index.
-func (index *ForestIndex) Insert(point Point) {
+func (index *ForestIndex) Insert(point Point, id int) {
 	// Apply hash functions.
-	//	hvs := index.Hash(point)
-
+	hvs := index.Hash(point)
+	for treeId, hv := range hvs {
+		index.trees[treeId].insertIntoTree(id, hv)
+	}
 }
+
+/*
+// Query searches for candidate keys given the signature
+// and writes them to an output channel
+func (index *ForestIndex) Query(q Point, out chan int) {
+	// Apply hash functions
+	hvs := index.Hash(q)
+	// Keep track of keys seen
+	seens := make(map[int]bool)
+	for i, tree := range index.trees {
+		if candidates, exist := table[hvs[i]]; exist {
+			for _, id := range candidates {
+				if _, seen := seens[id]; !seen {
+					seens[id] = true
+					out <- id
+				}
+			}
+		}
+	}
+}*/
