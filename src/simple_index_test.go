a4046cdbcbdf138c59f62e37d3dd3c306aa99f01
Charlie Mei <meicharlie@gmail.com>
2015-12-15 15:41:22 -0500

    Test refactoring

---
+++ src/simple_index_test.go
@@ -0,0 +1,93 @@
+package lsh
+
+import (
+	"sync"
+	"testing"
+)
+
+func Test_NewSimpleLsh(t *testing.T) {
+	lsh := NewSimpleLsh(5, 5, 100, 5.0)
+	if len(lsh.tables) != 5 {
+		t.Error("Lsh init fail")
+	}
+}
+
+func Test_Insert(t *testing.T) {
+	lsh := NewSimpleLsh(100, 5, 5, 5.0)
+	points := randomPoints(10, 100, 32.0)
+	for i, p := range points {
+		lsh.Insert(p, i)
+	}
+	for _, table := range lsh.tables {
+		if len(table) == 0 {
+			t.Error("Insert fail")
+		}
+	}
+}
+
+func Test_Query(t *testing.T) {
+	lsh := NewSimpleLsh(100, 5, 5, 5.0)
+	points := randomPoints(10, 100, 32.0)
+	insertedKeys := make([]int, 10)
+	for i, p := range points {
+		lsh.Insert(p, i)
+		insertedKeys[i] = i
+	}
+	// Use the inserted points as queries, and
+	// verify that we can get back each query itself
+	for i, key := range insertedKeys {
+		result := make(chan int)
+		go func() {
+			lsh.Query(points[i], result)
+			close(result)
+		}()
+		found := false
+		for foundKey := range result {
+			if foundKey == key {
+				found = true
+			}
+		}
+		if !found {
+			t.Error("Query fail")
+		}
+	}
+}
+
+func Test_ParallelQuery(t *testing.T) {
+	lsh := NewSimpleLsh(100, 5, 5, 5.0)
+	points := randomPoints(10, 100, 32.0)
+	for i, p := range points {
+		lsh.Insert(p, i)
+	}
+	// Run multiple queries in parallel
+	// and writing candidates to the same output
+	queries := randomPoints(10, 10, 32.0)
+	in := make(chan Point)
+	out := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(5)
+	// Input thread
+	go func() {
+		for _, q := range queries {
+			in <- q
+		}
+		close(in)
+	}()
+	// Worker threads
+	for w := 0; w < 5; w++ {
+		go func() {
+			for q := range in {
+				lsh.Query(q, out)
+			}
+			wg.Done()
+		}()
+	}
+	// Waiter thread
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	// Main thread collecting outputs
+	for _ = range out {
+	}
+}
