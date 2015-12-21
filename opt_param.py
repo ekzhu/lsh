72e9b3303093c14306f0ce429c8502b0727d7858
Eric Zhu <erkangzhu@gmail.com>
2015-12-21 16:21:04 -0500

    parameter optimization fix bug

--- opt_param.py
+++ opt_param.py
@@ -27,23 +27,23 @@ def _hash_probability(m, l, w, r):
         raise ValueError()
     return p
 
-def _recall(m, l, w, gamma_params, base, max_x):
+def _recall(m, l, w, gamma_params, max_x):
     k = len(gamma_params)
     s = 0.0
     for i in range(k):
         shape, loc, scale = gamma_params[i]
-        join_prob_func = lambda x : _hash_probability(m, l, w, np.sqrt(x)) * gamma.pdf(x, shape, loc, scale) * base
+        join_prob_func = lambda x : _hash_probability(m, l, w, np.sqrt(x)) * gamma.pdf(x, shape, loc, scale)
         prob, _ = quad(join_prob_func, 0.0, max_x) 
         s += prob
     return s / float(k)
 
-def _selectivity(m, l, w, gamma_param, base, max_x):
+def _selectivity(m, l, w, gamma_param, max_x):
     shape, loc, scale = gamma_param
-    join_prob_func = lambda x : _hash_probability(m, l, w, np.sqrt(x)) * gamma.pdf(x, shape, loc, scale) * base
+    join_prob_func = lambda x : _hash_probability(m, l, w, np.sqrt(x)) * gamma.pdf(x, shape, loc, scale)
     prob, _ = quad(join_prob_func, 0.0, max_x) 
     return prob
 
-def optimization(max_m, l, max_w, max_x, gamma_x, gamma_xk, required_recall, base_x, base_xk):
+def optimization(max_m, l, max_w, max_x, gamma_x, gamma_xk, required_recall):
     best_m = 0
     best_w = 0.0
     best_selectivity = float('inf')
@@ -52,25 +52,28 @@ def optimization(max_m, l, max_w, max_x, gamma_x, gamma_xk, required_recall, bas
         # Use binary search
         right_bound = max_w
         left_bound = 0.0
-        w = (right_bound - left_bound) / 2.0
+        w = (right_bound + left_bound) / 2.0
         delta = float('inf')
+        last_recall = float("inf")
         while delta > 1.0:
-            recall = _recall(m, l, w, gamma_xk, base_xk, max_x)
+            recall = _recall(m, l, w, gamma_xk, max_x)
             print("recall", recall)
             if recall < required_recall:
                 left_bound = w
             else:
                 right_bound = w
-            new_w = (right_bound - left_bound) / 2.0
+            print("New search interval", (left_bound, right_bound))
+            new_w = (right_bound + left_bound) / 2.0
             if new_w < 0.0:
                 print(left_bound, right_bound, w, new_w, m, l, recall)
                 raise ValueError()
             delta = np.abs(new_w - w)
             w = new_w
+            last_recall = recall
         if recall < required_recall - 0.01:
             print("Failed for l = %d m = %d is w =  %f, recall = %f" % (l, m, w, recall))
             continue
-        selectivity = _selectivity(m, l, w, gamma_x, base_x, max_x) 
+        selectivity = _selectivity(m, l, w, gamma_x, max_x) 
         print("Best for l = %d m = %d is w =  %f, recall = %f, selectivity = %f" % (l, m, w, recall, selectivity))
         if selectivity < best_selectivity:
             best_selectivity = selectivity
@@ -113,22 +116,19 @@ def load_topk_sample(datafile):
 all_pairs_sample = "./_image_all_pair_distance_sample"
 topk_sample = "./_image_query_distance_sample"
 k = 50
-max_w = 15000 
-max_m = 8
-required_recall = 0.9
-dataset_size = 10000
+max_w = 15000.0 
+max_m = 12
+required_recall = 0.5
 output = "opt_param_k_%d_recall_%.2f.json" % (k, required_recall)
-base_x = 1.0
-base_xk = 1.0
 
 gamma_x, dists_squared = load_all_pair_sample(all_pairs_sample)
 gamma_xk, topk_dists_squared = load_topk_sample(topk_sample)
 max_x = np.max(dists_squared)
-ls = [1, 4, 8, 16, 32]
+ls = [2, 4, 8, 16, 32, 64]
 out = []
 for l in ls:
     m, w = optimization(max_m, l, max_w, max_x, 
-            gamma_x, gamma_xk, required_recall, base_x, base_xk)
+            gamma_x, gamma_xk, required_recall)
     out.append({"L" : l, "M" : m, "W" : w})
-with open("opt_param.json", 'w') as f:
+with open(output, 'w') as f:
     json.dump(f, out)
